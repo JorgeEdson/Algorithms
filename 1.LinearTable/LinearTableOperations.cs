d02290cbb0ef1b5bc321e2653ab30bc865f012c9
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-03-25 23:42:22 -0300

    Add changes

--- 1.LinearTable/LinearTableOperations.cs
+++ 1.LinearTable/LinearTableOperations.cs
@@ -9,129 +9,154 @@ namespace _1.LinearTable
 {
     public static class LinearTableOperations
     {
-        public static void ShowLinearTable(int[] linearTableInput) 
+        public static void ShowLinearTable(int[] linearTableInput)
         {
             for (int i = 0; i < linearTableInput.Length; i++)
             {
                 if (i == linearTableInput.Length - 1)
-                    Console.Write("[" + i + "] = " + linearTableInput[i]+"\n");
+                    Console.Write("[" + i + "] = " + linearTableInput[i] + "\n");
                 else
                     Console.Write("[" + i + "] = " + linearTableInput[i] + ", ");
             }
         }
-        public static int[] AddNewValue(int[] linearTableInput, int newValue) 
+
+        #region Insert/Remove
+        public static int[] AddNewValue(int[] linearTableInput, int newValue)
         {
-            Console.WriteLine("\nAdd New Value -> "+newValue+"\n");
-            
-            int[] linearTableTemporary = new int[linearTableInput.Length+1];
-            
-            for (int i = 0; i < linearTableInput.Length;i++) 
+            Console.WriteLine("\nAdd New Value -> " + newValue + "\n");
+
+            int[] linearTableTemporary = new int[linearTableInput.Length + 1];
+
+            for (int i = 0; i < linearTableInput.Length; i++)
             {
                 linearTableTemporary[i] = linearTableInput[i];
             }
             linearTableTemporary[linearTableInput.Length] = newValue;
             return linearTableTemporary;
         }
-        public static int[] AddNewValueInIndex(int[] linearTableInput, int newValue, int index) 
+        public static int[] AddNewValueInIndex(int[] linearTableInput, int newValue, int index)
         {
-            Console.WriteLine("\nAdd New Value -> " + newValue+" In Index -> ["+index+"]\n");
+            Console.WriteLine("\nAdd New Value -> " + newValue + " In Index -> [" + index + "]\n");
 
             int[] linearTableTemporary = new int[linearTableInput.Length + 1];
 
             for (int i = 0; i < linearTableInput.Length; i++)
             {
-                if(i < index)
+                if (i < index)
                     linearTableTemporary[i] = linearTableInput[i];
                 else
                     linearTableTemporary[i + 1] = linearTableInput[i];
-            }            
+            }
             linearTableTemporary[index] = newValue;
             return linearTableTemporary;
         }
-        public static int[] DeleteValueInIndex(int[] linearTableInput,  int index) 
+        public static int[] DeleteValueInIndex(int[] linearTableInput, int index)
         {
             Console.WriteLine("\nDelete Index -> " + index + "\n");
 
             int[] linearTableTemporary = new int[linearTableInput.Length - 1];
-            for (int i = 0; i<linearTableInput.Length;i++) 
+            for (int i = 0; i < linearTableInput.Length; i++)
             {
                 if (i < index)
                     linearTableTemporary[i] = linearTableInput[i];
-                if(i>index)
-                    linearTableTemporary[i-1] = linearTableInput[i];
+                if (i > index)
+                    linearTableTemporary[i - 1] = linearTableInput[i];
             }
             return linearTableTemporary;
         }
-        public static void ShowMinValue(int[] linearTableInput)
-        {
-            Console.WriteLine("\nShowing the Min Value in Linear Table\n");
+        #endregion
 
+        #region Search Algorithms
+        public static int GetMinValueInLinearTable(int[] linearTableInput)
+        {
+            Console.WriteLine("\nCatching the Min Value in Linear Table\n");
             int minIndex = 0;
             for (int j = 1; j < linearTableInput.Length; j++)
             {
                 if (linearTableInput[minIndex] > linearTableInput[j])
                     minIndex = j;
             }
-            Console.WriteLine("[" + minIndex + "] = " + linearTableInput[minIndex]);
+            return linearTableInput[minIndex];
 
         }
-        public static void ShowIndexByValue(int[] linearTableInput, int value) 
+        public static int GetIndexByValue(int[] linearTableInput, int value)
         {
-            Console.WriteLine("\nSearching Index by Value in Linear Table\n");
+            Console.WriteLine("\nCatching Index by Value in Linear Table\n");
             bool isSearch = false;
-            for (int i = 0; i < linearTableInput.Length; i++) 
+            var result = -1;
+            for (int i = 0; i < linearTableInput.Length; i++)
             {
-                if (linearTableInput[i] == value) 
+                if (linearTableInput[i] == value)
                 {
                     isSearch = true;
-                    Console.WriteLine("Found -> [" + i + "] = " + value);
+                    result = i;
                     break;
                 }
             }
             if (!isSearch)
-                Console.WriteLine("The value "+value+" was not found.");
+                result = -1;
+            return result;
         }
-        public static void BubbleSorting(int[] linearTableInput) 
+        public static int GetIndexByValueWhithBinarySearch(int[] linearTableInput, int value)
+        {
+            Console.WriteLine("\nCatching Index by Value in Linear Table with Binary Search\n");
+            int minor = 0, half = 0, major = linearTableInput.Length - 1;
+            while (minor <= major)
+            {
+                half = (minor + major) / 2;
+                if (linearTableInput[half] == value)
+                    return half;
+                else if (linearTableInput[half] < value)
+                    minor = half + 1;
+                else if (linearTableInput[half] > value)
+                    minor = half - 1;
+            }
+            return -1;
+        }
+        #endregion
+
+        #region Sorting Algorithms 
+        public static void BubbleSorting(int[] linearTableInput)
         {
             Console.WriteLine("\nSorting Linear Table\n");
 
-            for (int i = 0;i< linearTableInput.Length-1;i++) 
+            for (int i = 0; i < linearTableInput.Length - 1; i++)
             {
                 bool isSwap = false;
-                for (int j=0;j< linearTableInput.Length-1;j++) 
+                for (int j = 0; j < linearTableInput.Length - 1; j++)
                 {
-                    if (linearTableInput[j]> linearTableInput[j+1]) 
-                    { 
+                    if (linearTableInput[j] > linearTableInput[j + 1])
+                    {
                         int flag = linearTableInput[j];
-                        linearTableInput[j] = linearTableInput[j+1];
+                        linearTableInput[j] = linearTableInput[j + 1];
                         linearTableInput[j + 1] = flag;
                         isSwap = true;
                     }
                 }
-                if (!isSwap)     
-                    break;              
-                
+                if (!isSwap)
+                    break;
+
             }
-        }        
+        }
         public static void SelectSorting(int[] linearTableInput)
         {
             Console.WriteLine("\nSelect Sorting Linear Table\n");
 
             int len = linearTableInput.Length - 1;
             int minIndex;
-            for (int i = 0;i<len;i++) 
+            for (int i = 0; i < len; i++)
             {
                 minIndex = i;
                 int minValue = linearTableInput[minIndex];
-                for (int j = i;j<len;j++) 
-                { 
-                    if(minValue > linearTableInput[j + 1]) 
-                    { 
-                        minValue = linearTableInput[j+1];
+                for (int j = i; j < len; j++)
+                {
+                    if (minValue > linearTableInput[j + 1])
+                    {
+                        minValue = linearTableInput[j + 1];
                         minIndex = j + 1;
                     }
                 }
-                if (i != minIndex) 
+                if (i != minIndex)
                 {
                     int temp = linearTableInput[i];
                     linearTableInput[i] = linearTableInput[minIndex];
@@ -139,37 +164,60 @@ namespace _1.LinearTable
                 }
             }
         }
-        public static void InsertSorting(int[] linearTableInput) 
+        public static void InsertSorting(int[] linearTableInput)
         {
             Console.WriteLine("\nInsert Sorting Linear Table\n");
 
-            for (int i = 0; i < linearTableInput.Length;i++) 
+            for (int i = 0; i < linearTableInput.Length; i++)
             {
                 int insertValue = linearTableInput[i];
                 int insertIndex = i;
-                for (int j = insertIndex-1;j>=0;j--) 
+                for (int j = insertIndex - 1; j >= 0; j--)
                 {
-                    if (insertValue < linearTableInput[j]) 
+                    if (insertValue < linearTableInput[j])
                     {
-                        linearTableInput[j+1] = linearTableInput[j];
+                        linearTableInput[j + 1] = linearTableInput[j];
                         insertIndex--;
                     }
                 }
                 linearTableInput[insertIndex] = insertValue;
             }
         }
-        public static void ReverseSorting(int[] linearTableInput) 
+        public static void ReverseSorting(int[] linearTableInput)
         {
             Console.WriteLine("\nReverse Sorting Linear Table\n");
-
             int lenght = linearTableInput.Length;
             int middle = lenght / 2;
-            for (int i =0;i<=middle;i++) 
+            for (int i = 0; i <= middle; i++)
             {
                 int temp = linearTableInput[i];
                 linearTableInput[i] = linearTableInput[lenght - i - 1];
                 linearTableInput[lenght - i - 1] = temp;
             }
         }
+        public static void ShellSorting(int[] linearTableInput)
+        {
+            Console.WriteLine("\nShell Sorting Linear Table\n");
+            for (int gap = linearTableInput.Length / 2; gap > 0; gap = gap / 2)
+            {
+                for (int i = gap; i < linearTableInput.Length; i++)
+                {
+                    int j = i;
+                    while (j - gap >= 0 && linearTableInput[j] < linearTableInput[j - gap])
+                    {
+                        Swap(linearTableInput, j, j - gap);
+                        j -= gap;
+                    }
+                }
+            }
+        }
+        public static void Swap(int[] array, int a, int b)
+        {
+            array[a] = array[a] + array[b];
+            array[b] = array[a] - array[b];
+            array[a] = array[a] - array[b];
+        }
+        #endregion
+
     }
 }
--- 1.LinearTable/Program.cs
+++ 1.LinearTable/Program.cs
@@ -1,16 +1,5 @@
 ﻿using _1.LinearTable;
 
-int[] linearTable = { 90, 70, 50, 80, 60 ,85};
-LinearTableOperations.ShowLinearTable(linearTable);
+int[] linearTable = { 90, 70, 50, 80, 60, 85 };
 
-//linearTable = LinearTableOperations.AddNewValue(linearTable, 75);
-//linearTable = LinearTableOperations.AddNewValueInIndex(linearTable, 75, 2);
-//linearTable = LinearTableOperations.DeleteValueInIndex(linearTable,2);
-//LinearTableOperations.ShowMinValue(linearTable);
-LinearTableOperations.ShowIndexByValue(linearTable, 60);
-//LinearTableOperations.BubbleSorting(linearTable);
-//LinearTableOperations.SelectSorting(linearTable);
-//LinearTableOperations.InsertSorting(linearTable);
-//LinearTableOperations.ReverseSorting(linearTable);
-LinearTableOperations.ShowLinearTable(linearTable);
-Console.ReadKey();
+//test methods here
